d40f8591c1c404ba4042f50f22b5f0f564d898a6
Pavithra-thiyagarajan <55681041+Pavithra-thiyagarajan@users.noreply.github.com>
2021-06-29 16:12:24 +0530

    Update StringOperation.swift

--- StringOperation.swift
+++ StringOperation.swift
@@ -22,3 +22,35 @@
   
   Result: ccc eee ddd fff
 */
+import Foundation
+import Glibc
+
+var students_name = ["Pavithra", "Archana", "zila","Dillipan","Kiran","Jayanthi","Vinothini","Naveena","Bharu","yamuna"]
+let index = [0,1,2,3,4,5,6,7,8,9]
+students_name.sort()    // 1.sorting an student array
+
+func splitAlternating(Index: [Int], splitAlternate:[String]) {
+    var alternateName1: [String] = [], alternateName2: [String] = []    
+
+    alternateName1.reserveCapacity(splitAlternate.count / 2 + 1)
+    alternateName2.reserveCapacity(splitAlternate.count / 2)
+    // 2.split the array into by alternate element
+    for iteration in Index {
+        if iteration % 2 == 0 {
+            alternateName1.append(splitAlternate[iteration])
+        }
+        else {
+           alternateName2.append(splitAlternate[iteration])
+        }
+    
+    }
+    // 3.remove the first element from both array
+    alternateName1.removeFirst()
+    alternateName2.removeFirst()
+    // 4.join the splitted array
+    let joined_array = alternateName1 + alternateName2
+    // 5.print the joined array
+    print(joined_array)
+
+}
+splitAlternating(Index : index , splitAlternate : students_name)
