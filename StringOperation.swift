babc479f898052701bed4fb27510677830ead0c0
Shanmugarajeshwaran R <47396623+Nova-Corp@users.noreply.github.com>
2021-06-29 11:44:03 +0530

    Create StringOperation.swift

---
+++ StringOperation.swift
@@ -0,0 +1,24 @@
+/*
+  1. Create the string array with 10 elements and sort it as Ascending order.
+  
+  let sudentsName = [ "aaa", "bbb", "ddd", "fff", "ccc", "eee"]
+  let sortedNames = [ "aaa", "bbb", "ccc", "ddd", "eee", "fff"]  
+  
+  2. Split that into 2 array by alternate element.
+  
+  let alternateName1 = ["aaa", "ccc", "eee"]
+  let alternateName2 = ["bbb", "ddd", "fff"]
+  
+  3. Remove the first element from both array.
+  
+  let newAlternateName1 = ["ccc", "eee"]
+  let newAlternateName1 = ["ddd", "fff"]
+  
+  4. Join the splited array.
+  
+  let joinedArray = ["ccc", "eee", "ddd", "fff"]
+  
+  5. Print the new array as string.
+  
+  Result: ccc eee ddd fff
+*/
