e44c62ae8c0fb26b2a95c02a960676efebe2e57e
Shanmugarajeshwaran R <47396623+Nova-Corp@users.noreply.github.com>
2021-06-30 09:52:22 +0530

    Create DictionaryOperation.swift

---
+++ DictionaryOperation.swift
@@ -0,0 +1,30 @@
+/*
+  1. Create the array with 10 dictionary element.
+      Example:-
+        let studentDatabase: [NSDictionary] = [
+                                ["name": "aaa",
+                                  "college": "bbb",
+                                  "department": "ccc"],
+                                ["name": "aaa1",
+                                  "college": "bbb1",
+                                  "department": "ccc1"],
+                                ["name": "aaa2",
+                                  "college": "bbb2",
+                                  "department": "ccc2"],
+                                ["name": "aaa3",
+                                  "college": "bbb3",
+                                  "department": "ccc3"],
+                                ["name": "aaa4",
+                                  "college": "bbb4",
+                                  "department": "ccc4"],
+                                ["name": "aaa5",
+                                  "college": "bbb5",
+                                  "department": "ccc5"],
+                              ]
+  2. Extract the name, college, department as separate array
+      let studentName = ["aaa", "aaa1", ...]
+      let collegeName = ["bbb", "bbb1", ...]
+      let departmentName = ["ccc", "ccc1", ...]
+
+
+*/
