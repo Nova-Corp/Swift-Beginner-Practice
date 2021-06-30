d89d2ec926b7934d160bcf0d651d0dc370d3f0ce
Pavithra-thiyagarajan <55681041+Pavithra-thiyagarajan@users.noreply.github.com>
2021-06-30 13:31:51 +0530

    Update DictionaryOperation.swift

--- DictionaryOperation.swift
+++ DictionaryOperation.swift
@@ -28,3 +28,62 @@
 
 
 */
+import Foundation
+//1. Create the array with 10 dictionary element.
+var studentDatabase: [[String:String?]] = [
+                                ["name": "Pavithra",
+                                  "college": "Sri Eshwar College",
+                                  "department": "ECE"],
+                                ["name": "Archana",
+                                  "college": "Karunya University",
+                                  "department": "Bio-Technology"],
+                                ["name": "Kirthick",
+                                  "college": "KSR College",
+                                  "department": "CSE"],
+                                ["name": "Kiran",
+                                  "college": "SRM College",
+                                  "department": "B.Com"],
+                                ["name": "Bharathi",
+                                  "college": "Karpagam",
+                                  "department": "B.Sc"],
+                                ["name": "Kaviya",
+                                  "college": "VRS College",
+                                  "department": "M.com"],
+                                ["name": "Shalini",
+                                  "college": "Sri Ramakrishna College",
+                                  "department": "B.Com"],
+                                ["name": "Harish",
+                                  "college": "SRA College",
+                                  "department": "BBA"],
+                                ["name": "Raji",
+                                  "college": "Karpagam University",
+                                  "department": "B.Sc"],
+                                ["name": "Nila",
+                                  "college": "KPR College",
+                                  "department": "M.com"],
+                              ]
+var studentName = [String]()
+var collegeName = [String]()
+var departmentName = [String]()
+//2.Extract the name, college, department as separate array
+studentDatabase.map{item in
+    if let studentNames = item["name"] {
+        if(studentNames != nil){
+            studentName.append(studentNames!)
+        }
+    }
+    if let collegeNames = item["college"] {
+        if(collegeNames != nil){
+            collegeName.append(collegeNames!)
+        }
+    }
+    if let departmentNames = item["department"] {
+        if(departmentNames != nil){
+            departmentName.append(departmentNames!)
+        }
+    }
+    return;
+}
+print(studentName)
+print(collegeName)
+print(departmentName)
